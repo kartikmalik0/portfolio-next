18165792e1673b4f30055099497089ad214355c4
Kartik <kartik20044@gmail.com>
2024-04-17 07:54:29 -0300

    card design fixed on mobile and mobileR image edited

--- src/app/components/ui/card-stack.tsx
+++ src/app/components/ui/card-stack.tsx
@@ -39,12 +39,12 @@ export const CardStack = ({
   };
 
   return (
-    <div className="relative  h-60 w-[22rem] md:h-60 md:w-96">
+    <div className="relative  h-60 w-[20rem] md:h-60 md:w-96">
       {cards.map((card, index) => {
         return (
           <motion.div
             key={card.id}
-            className="absolute  dark:bg-black bg-white h-60 w-[22rem] md:h-60 md:w-96 p-4 rounded-3xl  shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+            className="absolute  dark:bg-black bg-white h-60 w-[20rem] md:h-60 md:w-96 p-4 rounded-3xl  shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
             style={{
               transformOrigin: "top center",
             }}
