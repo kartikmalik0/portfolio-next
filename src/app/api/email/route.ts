8e12ed7bf33309823fc16711bafc64b75f2a09b2
Kartik <kartik20044@gmail.com>
2024-06-01 14:13:03 -0300

    contact form issue fixed

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,25 +1,25 @@
-import { NextRequest } from 'next/server';
-import { Resend } from 'resend';
+import { NextRequest } from "next/server";
+import { Resend } from "resend";
 
-const resend = new Resend("re_3u6gNSmP_AihzwZfJ7hRpAP7PwsXhEp8D");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req:NextRequest){
-   const body = await req.json();
+export async function POST(req: NextRequest) {
+    const body = await req.json();
 
-   const htmlBody = `
+    const htmlBody = `
             <p>You have received a new message from the portfolio contact form:</p>
             <p><strong>Name:</strong> ${body?.name}</p>
             <p><strong>Email:</strong> ${body?.email}</p>
             <p><strong>Message:</strong> ${body?.message}</p>
         `;
-    const {data , error} = await resend.emails.send({
-        from:"Portfolio <onboarding@resend.dev>",
-        to:["kartik20044@gmail.com"],
-        subject:'Contact Mail From Portfolio',
-        html:htmlBody
-    })
-    if(error){
-        return Response.json(error)
+    const { data, error } = await resend.emails.send({
+        from: "Portfolio <onboarding@resend.dev>",
+        to: ["kartik20044@gmail.com"],
+        subject: "Contact Mail From Portfolio",
+        html: htmlBody,
+    });
+    if (error) {
+        return Response.json(error);
     }
-    return Response.json({message:"Email sent successfully"})
+    return Response.json({ message: "Email sent successfully" });
 }
